fa18495aaf2425969b194414774902b037d43d02
Dylan <dylanfontenot@protonmail.com>
2021-09-28 18:52:48 -0500

    local storage

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var saveBtn = $(".saveBtn")
 saveBtn.each(function(){
     $(this).on("click",function(event){
 var text = $(this).siblings(".description").val()
+var hour = $(this).siblings(".hour").text()
 console.log(text)
+localStorage.setItem(hour, text)
     })
 }) 
+$("#9AM").val(localStorage.getItem("9AM"))
