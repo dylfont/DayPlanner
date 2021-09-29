0a0989a23f4f72f2b11efe5b364e32a3c7677430
Dylan <dylanfontenot@protonmail.com>
2021-09-28 19:22:33 -0500

    blocks in html and js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-console.log ("hello")
+console.log("hello")
 var currentday = moment().format('MMMM Do YYYY');
-var timeEl=$("#currentDay")
+var timeEl = $("#currentDay")
 timeEl.text(currentday)
 var saveBtn = $(".saveBtn")
-saveBtn.each(function(){
-    $(this).on("click",function(event){
-var text = $(this).siblings(".description").val()
-var hour = $(this).siblings(".hour").text()
-console.log(text)
-localStorage.setItem(hour, text)
+saveBtn.each(function () {
+    var id = $(this).siblings(".hour").text()
+    $(this).siblings(".description").val(localStorage.getItem(id))
+    var current = moment().hours()
+    var time = $(this).siblings(".description").attr("id")
+    if(time<current){
+        $(this).siblings(".description").addClass("past")
+    }
+    $(this).on("click", function (event) {
+        var hour = $(this).siblings(".hour").text()
+        var text = $(this).siblings(".description").val()
+        console.log(text)
+        localStorage.setItem(hour, text)
     })
-}) 
-$("#9AM").val(localStorage.getItem("9AM"))
+})
+
